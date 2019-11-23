559ebb2e5cef8bd7796739cea387c3db1be1303f
imattf <imattf@pm.me>
2019-11-23 10:56:51 -0800

    added ninj0102

---
+++ ninj0102.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main() {
+	//x = 42
+	//y = "James Bond"
+	//z = true
+
+	fmt.Println("x is:", x)
+	fmt.Println("y is:", y)
+	fmt.Println("z is:", z)
+}
+
+// The Zero Value comes from compiler
+
