6ba0002283d95e144148d7a70cc2863f360dcc7f
imattf <imattf@pm.me>
2019-12-31 10:44:42 -0800

    added ninj0611

--- ninj0611.go
+++ ninj0611.go
@@ -1,5 +1,4 @@
 // The best way to learn is to teach. For this hands-on exercise,
-
 //     choose one of the above exercises, or use the recursion example of factorial
 //     download, install, and get it running
 //     https://obsproject.com/
@@ -7,3 +6,31 @@
 //     upload the video to youtube
 // 	share the video on twitter and tag me in it
 // 	( https://twitter.com/Todd_McLeod ) so that I can see it!
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hey...")
+	a := foo()
+
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+
+	b := foo()
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func foo() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
