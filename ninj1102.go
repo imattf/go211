18ac62ec989da48b903a4c273f619b88b83f29c9
imattf <imattf@pm.me>
2020-01-10 13:12:18 -0800

    added ninj1102

---
+++ ninj1102.go
@@ -0,0 +1,47 @@
+// Start with this code. Create a custom error message using “fmt.Errorf”.
+// solution:
+//     https://play.golang.org/p/HugU4HJEEO
+//     https://play.golang.org/p/NII-lmGasj
+//     https://play.golang.org/p/Vo5kIoR-sG
+
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+		log.Panicln(err)
+
+	}
+
+	fmt.Println(string(bs), err)
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		//return bs, fmt.Errorf("norgate math again: square root of negative number: %v", err)
+		return bs, errors.New(fmt.Sprintf("norgate math again: square root of negative number", err))
+	}
+	return bs, nil
+}
