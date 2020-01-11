b28a93ca835c2dba8af9d84450f0efcfc63a96e9
imattf <imattf@pm.me>
2020-01-10 18:49:10 -0800

    removed package dog

--- dog/main.go
+++
@@ -1,8 +0,0 @@
-//Package dog takes human years and turns them into dog years (1 human year = 7 dog years)
-package dog
-
-//Years takes an integer of human years and returns an integer of dog years
-func Years(i int) int {
-	dogYears := i * 7
-	return dogYears
-}
